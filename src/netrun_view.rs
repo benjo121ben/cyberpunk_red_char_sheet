225db901bde31488f073fcdc141b318c71d2e608
Benjamin Wenger <benjamin.wenger00@gmail.com>
2025-06-29 22:27:57 +0200

    netrun

--- src/netrun_view.rs
+++ src/netrun_view.rs
@@ -1,4 +1,4 @@
-use cp_char_data::gear::GearData;
+use cp_char_data::gear::{get_map_key, GearData};
 use leptos::prelude::*;
 
 use crate::{help::{get_char_signal_from_ctx, reduce_or_remove_items_in_map}, info_modal_view::SimpleModalData};
@@ -8,14 +8,16 @@ pub fn NetrunView() -> impl IntoView {
     let char_signal = get_char_signal_from_ctx();
     let gear_data: GearData = use_context().expect("expecting gear data to exist. | NetrunView");
     let simple_modal_signal: RwSignal<SimpleModalData> = use_context().expect("allitemsview: simple modal should exist");
+    let interface_modal = Memo::new(move|_| {char_signal.read().role_abilities.netrunner});
     view! {
         <div class="netrun_view">
+            <span>{move || format!("Interface {} - Actions: {}", interface_modal(), (interface_modal() as f32 / 3.0).ceil() + 1.0)}</span>
             <For each=move|| {char_signal.read().programs.clone().into_iter().map(|(key, _)| key).collect::<Vec<_>>()}
                 key=move|key| key.clone()
                 children=move|key| {
                     let find_item = StoredValue::new(gear_data.programs.iter().find(|find_item| {
-                        let changed_name = find_item.name.to_lowercase().replace(" ", "_");
-                        changed_name == key
+                        let map_key = get_map_key(*find_item);
+                        map_key == key
                     }).cloned().expect("expecting item to exist"));
                     let key_clone = key.clone(); 
                     let item_amount_memo = Memo::new(move |_| {char_signal.read().programs.get(&key_clone).expect("expecting item amount to exist").to_string()});
@@ -47,7 +49,41 @@ pub fn NetrunView() -> impl IntoView {
                             <button on:click=move|ev|{
                                 ev.stop_propagation();
                                 char_signal.update(|c| {
-                                    reduce_or_remove_items_in_map(&mut c.gear, &key)
+                                    reduce_or_remove_items_in_map(&mut c.programs, &key)
+                                })
+                            }>
+                                X
+                            </button>
+                        </div>
+                    }
+                }
+            />
+            <hr style="width:90%"/>
+            <For each=move|| {char_signal.read().cyberdeck_hardware.clone().into_iter().map(|(key, _)| key).collect::<Vec<_>>()}
+                key=move|key| key.clone()
+                children=move|key| {
+                    let find_item = StoredValue::new(gear_data.cyberdeck_hardware.iter().find(|find_item| {
+                        let map_key = get_map_key(*find_item);
+                        map_key == key
+                    }).cloned().expect("expecting item to exist"));
+                    let key_clone = key.clone(); 
+                    let item_amount_memo = Memo::new(move |_| {char_signal.read().cyberdeck_hardware.get(&key_clone).expect("expecting item amount to exist").to_string()});
+                    view! {
+                        <div class="gear_view program_view"
+                            on:click=move|_| simple_modal_signal.update(|data| {
+                                data.title = find_item.get_value().name.clone();
+                                data.description = find_item.get_value().description.clone();
+                                data.show();
+                            })
+                        >
+                            <span>
+                                {move || item_amount_memo()}x
+                                {move || find_item.get_value().name.to_string()}
+                            </span>
+                            <button on:click=move|ev|{
+                                ev.stop_propagation();
+                                char_signal.update(|c| {
+                                    reduce_or_remove_items_in_map(&mut c.cyberdeck_hardware, &key)
                                 })
                             }>
                                 X
